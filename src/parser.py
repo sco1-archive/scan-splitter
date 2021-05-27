93341c3060d8a6f8fdc9b4186e33cd2625c89bf8
S. Co1 <sco1.git@gmail.com>
2021-05-27 11:37:55 -0400

    Convert split files to CSVs

--- src/parser.py
+++ src/parser.py
@@ -1,26 +1,58 @@
+import re
 from pathlib import Path
 
 from rich import print as rprint
 
+# Matches a positive/negative float preceeded by whitespace
+FLOAT_PATTERN = r"\s([-+]?\d*\.\d+)"
+
 
 def _clean_line(line: str) -> str:
-    """ """
+    """
+    Clean out undesired elements from the provided scan data row.
+
+    The following cleanups are performed:
+        1. Remove leading `0` or `1` validity flags
+        2. Strip leading & trailing whitespace
+        3. Remove colons (`:`)
+        4. Replace tabs with spaces
+
+    NOTE: Rows are considered comments if they begin with an asterix (`*`) after steps 1 & 2; if a
+    comment row is encountered, this function short-circuits and returns the partially cleaned line
+    """
     # All non-header lines are assumed to lead off with a validitity flag (0 or 1) that we can
     # strip off, if present
     line = line.removeprefix("1").removeprefix("0")
     line = line.strip()
 
     # Short circuit on comment lines because they may not have the same format as data rows
+    # This needs to be done after stripping the prefix, since comment lines have the flag
     if line.startswith("*"):
         return line
 
-    # Remove any colons from the line
     line = line.replace(":", "")
-
-    # Replace tabs
     line = line.replace("\t", " ")
 
     return line
+
+
+def _line2csv(line: str) -> str:
+    """
+    Convert the provided line (assumed to be cleaned) into a CSV row.
+
+    Input lines are assumed to be a string variable name followed by one or more float values. The
+    line is assumed to be space-delimited, and the variable name column may include spaces.
+    """
+    # Use the measurement pattern to split on the first floating point number
+    measurement_name, *measurements = re.split(FLOAT_PATTERN, line, maxsplit=1)
+
+    # Put the measurements back together, trim, then replace any whitespace with commas
+    joined_measurements = " ".join(measurements).strip()
+    joined_measurements = re.sub(r"\s{2,}", " ", joined_measurements)  # Collapse whitespace
+    joined_measurements = joined_measurements.replace(" ", ",")
+
+    new_line = f"{measurement_name},{joined_measurements}"
+    return new_line
 
 
 def split_composite_file(composite_src: list[str]) -> tuple[list[str], list[str]]:
@@ -62,7 +94,7 @@ def split_composite_file(composite_src: list[str]) -> tuple[list[str], list[str]
             # Discard comments
             continue
 
-        chunk.append(line)
+        chunk.append(_line2csv(line))
     else:
         # Append the last chunk when we finish reading the file
         out_chunks.append(chunk)
@@ -75,22 +107,22 @@ def split_composite_file(composite_src: list[str]) -> tuple[list[str], list[str]
 
 def file_pipeline(in_file: Path) -> None:
     """
-    Split the provided composite file into its anthro & landmark components.
+    Split the provided composite file into CSVs of its anthro & landmark components.
 
     Split files are written to the same directory as the input file:
         * Anthro components append ".anthro" to the name of the file
         * Landmark components append ".lmk" to the name of the file
 
     e.g. `./some_scan_composite.txt` becomes:
-        `./some_scan_composite.anthro.txt` and `./some_scan_composite.lmk.txt`
+        `./some_scan_composite.anthro.csv` and `./some_scan_composite.lmk.csv`
 
     Comments (lines containing `*`) and header lines (lines beginning with `#`) are discarded
 
     NOTE: Any existing anthro & landmark files will be overwritten
     """
     base_stem = in_file.stem
-    anthro_filepath = in_file.with_stem(f"{base_stem}.anthro")
-    landmark_filepath = in_file.with_stem(f"{base_stem}.lmk")
+    anthro_filepath = in_file.with_name(f"{base_stem}.anthro.csv")
+    landmark_filepath = in_file.with_name(f"{base_stem}.lmk.csv")
 
     rprint(f"Processing {base_stem!r} ... ", end="")
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,66 +1,8 @@
+import itertools
 from textwrap import dedent
 
 import pytest
 from src import parser
-
-
-COMPOSITE_TEST_CASES = [
-    (
-        dedent(  # Check that headers are discarded; anthro is joined
-            """\
-            #SizeStream Measurements
-            #Stored on Tue May 18 06:49:24 2021
-            #SizeStream Core Measurements
-            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
-            #
-            1  Actual Weight: 1.2
-            #SizeStream Custom Measurements
-            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
-            #
-            1  Chest: 3.4
-            #SizeStream Landmarks
-            #format - Landmarks Valid (1 = valid), Landmark Name, Landmark x y z
-            #
-            1  AbdomenBack	5.6	7.8	-9.10
-            """
-        ),
-        ["Actual Weight 1.2", "Chest 3.4"],
-        ["AbdomenBack 5.6 7.8 -9.10"],
-    ),
-    (
-        dedent(  # Check that comments are discarded
-            """\
-            #SizeStream Measurements
-            #Stored on Tue May 18 06:49:24 2021
-            #SizeStream Core Measurements
-            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
-            #
-            1  Actual Weight: 1.2
-            #SizeStream Custom Measurements
-            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
-            #
-            1  *****  Body Fat / Fitness: *****
-            1  Chest: 3.4
-            #SizeStream Landmarks
-            #format - Landmarks Valid (1 = valid), Landmark Name, Landmark x y z
-            #
-            1  AbdomenBack	5.6	7.8	-9.10
-            """
-        ),
-        ["Actual Weight 1.2", "Chest 3.4"],
-        ["AbdomenBack 5.6 7.8 -9.10"],
-    ),
-]
-
-
-@pytest.mark.parametrize(("raw_src", "truth_anthro", "truth_landmark"), COMPOSITE_TEST_CASES)
-def test_composite_file_parsing(  # noqa: D103
-    raw_src: str, truth_anthro: list[str], truth_landmark: list[str]
-) -> None:
-    anthro, landmark = parser.split_composite_file(raw_src.splitlines())
-
-    assert anthro == truth_anthro
-    assert landmark == truth_landmark
 
 
 LINE_CLEANER_TEST_CASES = [
@@ -96,6 +38,123 @@ LINE_CLEANER_TEST_CASES = [
 
 
 @pytest.mark.parametrize(("raw_line", "truth_line"), LINE_CLEANER_TEST_CASES)
-def test_line_cleaning(raw_line: str, truth_line: str) -> None:
-    """ """
+def test_line_cleaning(raw_line: str, truth_line: str) -> None:  # noqa: D103
     assert parser._clean_line(raw_line) == truth_line
+
+
+LINE2CSV_TEST_CASES = [
+    (
+        "Halter 1.2",
+        "Halter,1.2",
+    ),
+    (
+        "halter 1.2",
+        "halter,1.2",
+    ),
+    (
+        "Actual Weight 1.2",
+        "Actual Weight,1.2",
+    ),
+    (
+        "Chest/Bust Circum Back Left 1.2",
+        "Chest/Bust Circum Back Left,1.2",
+    ),
+    (
+        "ActualWeight 1.2",
+        "ActualWeight,1.2",
+    ),
+    (
+        "Chest / Bust Circum Tape Measure 1.2",
+        "Chest / Bust Circum Tape Measure,1.2",
+    ),
+    (
+        "Body Fat (men) 1.2",
+        "Body Fat (men),1.2",
+    ),
+    (
+        "Waist at 50% 1.2",
+        "Waist at 50%,1.2",
+    ),
+    (
+        "Bust60DegreesLeft 1.2 1.2 1.2",
+        "Bust60DegreesLeft,1.2,1.2,1.2",
+    ),
+    (
+        "AbdomenBack 5.6 7.8 -9.10",
+        "AbdomenBack,5.6,7.8,-9.10",
+    ),
+    (
+        "Bust With Drop Back 5.6 7.8 -9.10",
+        "Bust With Drop Back,5.6,7.8,-9.10",
+    ),
+    (
+        "Right heel 5.6 7.8 -9.10",
+        "Right heel,5.6,7.8,-9.10",
+    ),
+]
+
+
+@pytest.mark.parametrize(("raw_line", "truth_csv"), LINE2CSV_TEST_CASES)
+def test_line2csv(raw_line: str, truth_csv: str) -> None:  # noqa: D103
+    assert parser._line2csv(raw_line) == truth_csv
+
+
+COMPOSITE_TEST_CASES = [
+    (
+        dedent(  # Check that headers are discarded; anthro is joined
+            """\
+            #SizeStream Measurements
+            #Stored on Tue May 18 06:49:24 2021
+            #SizeStream Core Measurements
+            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
+            #
+            1  Actual Weight: 1.2
+            #SizeStream Custom Measurements
+            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
+            #
+            1  Chest: 3.4
+            #SizeStream Landmarks
+            #format - Landmarks Valid (1 = valid), Landmark Name, Landmark x y z
+            #
+            1  AbdomenBack	5.6	7.8	-9.10
+            """
+        ),
+        ["Actual Weight,1.2", "Chest,3.4"],
+        ["AbdomenBack,5.6,7.8,-9.10"],
+    ),
+    (
+        dedent(  # Check that comments are discarded
+            """\
+            #SizeStream Measurements
+            #Stored on Tue May 18 06:49:24 2021
+            #SizeStream Core Measurements
+            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
+            #
+            1  Actual Weight: 1.2
+            #SizeStream Custom Measurements
+            #format - Measurement Valid (1 = valid), Measurement Name, Measurement
+            #
+            1  *****  Body Fat / Fitness: *****
+            1  Chest: 3.4
+            #SizeStream Landmarks
+            #format - Landmarks Valid (1 = valid), Landmark Name, Landmark x y z
+            #
+            1  AbdomenBack	5.6	7.8	-9.10
+            """
+        ),
+        ["Actual Weight,1.2", "Chest,3.4"],
+        ["AbdomenBack,5.6,7.8,-9.10"],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    ("raw_src", "truth_anthro", "truth_landmark"), COMPOSITE_TEST_CASES, ids=itertools.count()
+)
+def test_composite_file_parsing(  # noqa: D103
+    raw_src: str, truth_anthro: list[str], truth_landmark: list[str]
+) -> None:
+    anthro, landmark = parser.split_composite_file(raw_src.splitlines())
+
+    assert anthro == truth_anthro
+    assert landmark == truth_landmark
